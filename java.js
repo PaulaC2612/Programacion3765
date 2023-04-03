cb16ed36b646bb71866fd9cd5610dff6902b9469
andreacortes2612@hotmail.com <andreacortes2612@hotmail.com>
2023-04-03 09:28:03 -0500

    Añadi pagina web

---
+++ java.js
@@ -0,0 +1,12 @@
+document.write("Salud y belleza")
+
+var datos= document.getElementById("datos")
+var nombre = "Paula Andrea Cortes"
+var edad = 25
+
+document.write("Salud y belleza")
+
+datos.innerHTML=
+<h2>Salud y belleza</h2>
+<h3>Tips</h3>
+<h3>Edad: ${edad}</h3>
